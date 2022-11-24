5fd2faaa56be5dedd91e516a629b9a72d339061c
Raíssa Santos <raissa.santos.095@ufrn.edu.br>
2022-11-24 01:59:46 -0300

    feat: inicialização da rota solução

--- src/pcvq/Pcvq.java
+++ src/pcvq/Pcvq.java
@@ -1,5 +1,27 @@
 package src.pcvq;
 
 public class Pcvq {
+    public static void findRoute(Graph graphReceived, Route route) {
+        Graph graphAttempted = new Graph(1);
+        addHotel(graphReceived, graphAttempted);
+        Integer scoreSatisfaction = 0;
+        Integer distance = 0;
+        // heuristic
+        addHotel(graphReceived, graphAttempted);
+
+        route.setRoute(graphAttempted.getAttractions());
+        route.setScoreSatisfaction(scoreSatisfaction);
+        route.setDistance(distance);
+    }
+
     
+
+    /** 
+     * Add hotel into the route.
+     * @param graphReceived Graph received with all attractions.
+     * @param graphAttempted Graph with attractions to build the final route.
+     */
+    private static void addHotel(Graph graphReceived, Graph graphAttempted) {
+        graphAttempted.addAttraction(graphReceived.getAttractions().get(0));
+    }
 }
